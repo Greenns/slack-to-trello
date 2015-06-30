eed9f981af422bafe08ff7bf1f4092d86c3cf6b6
MrAllerre <mrallerre@gmail.com>
2015-06-30 20:04:12 +0200

    Test retour à la ligne

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post('/*', function(req, res, next) {
         var url = data.shortUrl;
 
         console.log('> ' + name);
-        sendNotif('['+user+'] ' + name);
+        sendNotif('['+user+'] \n' + name);
 
         res.status(200).send('Bug "' + name + '" signal\351 ici: <' + url + '>');
     });
