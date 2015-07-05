d5d85c7427d071020e3f048d27e35545ef96d14b
MrAllerre <mrallerre@gmail.com>
2015-07-05 15:22:35 +0200

    Fix attachments

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ function postToTrello(listId, command, text, cb) {
 
 function sendNotif(msg, user) {
      slack.send({
-          text: 'Nouveau bug signalé',
+          text: 'Nouveau bug signalé:',
           channel: getChannel,
           username: 'UHBot',
-          attachments: [{"pretext": "Description:", "text": "" + msg + "", "color":"#F35A00", "fields": [{"title": "Signalé par:", "value": "" + user + "", "short": "false"}, {"title": "Priorité:", "value": "Indéterminé", "short": "true"}]}],
+          attachments: [{"pretext": "", "text": "" + msg + "", "color":"#F35A00", "fields": [{"title": "Signalé par:", "value": "" + user + "", "short": "false"}, {"title": "Priorité:", "value": "Indéterminé", "short": "true"}]}],
           icon_url: 'http://img15.hostingpics.net/pics/834337TechnicalSupport64.png'
      });
 }
